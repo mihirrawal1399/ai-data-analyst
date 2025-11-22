4554ced9367ffc60e6da394c4a419f25da17027d
Mihir <mihirrawal1399@gmail.com>
2025-11-22 22:09:40 +0530

    setup process env for api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.API_PORT ?? 4000);
+  console.log(`API Application is running on ${await app.getUrl()}`);
 }
 bootstrap();
